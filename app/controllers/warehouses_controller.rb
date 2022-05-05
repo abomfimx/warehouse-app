3d6bdc3e724407bbb12d497376d1d14f1c60ebe4
Andre Bomfim <abomfimx@gmail.com>
2022-05-04 22:19:25 -0300

    Usuario cadastra um galpão

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -3,12 +3,22 @@ class WarehousesController < ApplicationController
     @warehouse = Warehouse.find(params[:id])
   end
 
+  def new
+    @warehouse = Warehouse.new
+  end
 
+  def create
+    @warehouse = Warehouse.new(warehouse_params)
+    if @warehouse.save
+      return redirect_to root_path, notice: 'Galpão cadastrado com sucesso'
+    end
+    render :new
+  end
 
 
   private 
 
   def warehouse_params
-
+    params.require(:warehouse).permit(:name, :code, :city, :ddescription, :address,:cep, :area)
   end
 end
