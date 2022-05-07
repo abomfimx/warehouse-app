d8a6aa3bfee91cd68ebce66aacdd4a2297ecc9b4
Andre Bomfim <abomfimx@gmail.com>
2022-05-07 17:55:54 -0300

    Adicionado testes unitários

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,3 +1,7 @@
 class Warehouse < ApplicationRecord
-  validates :name, :code, :city, :area, :cep, :description, presence: true
+  validates :name, :code, :address, :cep, :city, :area, :description, presence: true
+  validates :name, uniqueness: true
+  validates :code, uniqueness: true
+  validates :code, length: { is: 3 }
+  validates :cep, format: { with: /\A[0-9]{5}-[0-9]{3}\z/, message: ' - formato inválido' }
 end
--- spec/models/warehouse_spec.rb
+++ spec/models/warehouse_spec.rb
@@ -1,5 +1,140 @@
 require 'rails_helper'
 
 RSpec.describe Warehouse, type: :model do
+  describe '#valid?' do
+    context 'presence' do
+      it 'falso quando Nome está vazio' do
+        # Arrange
+        warehouse = Warehouse.new(name: '', code: 'RIO', address:'Av. Atlantica, 1200',
+                                  cep: '25000-000', city: 'Rio', area: 40000,
+                                  description: 'Galpão do aeroporto do Rio')
+                                
+        # Act
+        result = warehouse.valid?
+        # Assert
+        expect(result).to eq false
+      end
 
+      it 'falso quando Código está vazio' do
+        # Arrange
+        warehouse = Warehouse.new(name: 'Rio', code: '', address:'Av. Atlantica, 1200',
+                                  cep: '25000-000', city: 'Rio', area: 40000,
+                                  description: 'Galpão do aeroporto do Rio')
+                                
+        # Act
+        result = warehouse.valid?
+        # Assert
+        expect(result).to eq false
+      end
+
+      it 'falso quando Endereço está vazio' do
+        # Arrange
+        warehouse = Warehouse.new(name: 'Rio', code: 'RIO', address: '' ,
+                                  cep: '25000-000', city: 'Rio', area: 40000,
+                                  description: 'Galpão do aeroporto do Rio')
+                                
+        # Act
+        result = warehouse.valid?
+        # Assert
+        expect(result).to eq false
+      end
+
+      it 'falso quando CEP está vazio' do
+        # Arrange
+        warehouse = Warehouse.new(name: 'Rio', code: 'RIO', address: 'Av. Atlantica, 1200' ,
+                                  cep: '', city: 'Rio', area: 40000,
+                                  description: 'Galpão do aeroporto do Rio')
+                                
+        # Act
+        result = warehouse.valid?
+        # Assert
+        expect(result).to eq false
+      end
+
+      it 'falso quando Cidade está vazio' do
+        # Arrange
+        warehouse = Warehouse.new(name: 'Rio', code: 'RIO', address: 'Av. Atlantica, 1200' ,
+                                  cep: '25000-000', city: '', area: 40000,
+                                  description: 'Galpão do aeroporto do Rio')
+                                
+        # Act
+        result = warehouse.valid?
+        # Assert
+        expect(result).to eq false
+      end
+
+      it 'falso quando Área está vazio' do
+        # Arrange
+        warehouse = Warehouse.new(name: 'Rio', code: 'RIO', address: 'Av. Atlantica, 1200' ,
+                                  cep: '25000-000', city: 'Rio', area: '',
+                                  description: 'Galpão do aeroporto do Rio')
+                                
+        # Act
+        result = warehouse.valid?
+        # Assert
+        expect(result).to eq false
+      end
+      
+      it 'falso quando Descrição está vazio' do
+        # Arrange
+        warehouse = Warehouse.new(name: 'Rio', code: 'RIO', address: 'Av. Atlantica, 1200' ,
+                                  cep: '25000-000', city: 'Rio', area: '40000',
+                                  description: '')
+                                
+        # Act
+        result = warehouse.valid?
+        # Assert
+        expect(result).to eq false
+      end
+
+    end
+
+    it 'falso quando código já está em uso' do
+      # Arrangge
+      first_warehouse = Warehouse.create(name: 'Brasília', code: 'BRA',address:'Endereço', 
+                                      cep: '95000-000', city: 'Brasilia', area: 1000,
+                                      description: 'Alguma coisa')
+                                    
+      second_warehouse = Warehouse.new(name: 'Niteroi', code: 'BRA',address:'Avenida', 
+                                        cep: '35000-000', city: 'Niteroi', area: 1500,
+                                        description: 'Outra coisa')
+
+      # Act
+      result = second_warehouse.valid?
+      # Assert
+      expect(result).to eq false
+
+    end
+
+    it 'falso quando nome já está em uso' do
+      # Arrangge
+      first_warehouse = Warehouse.create(name: 'Recife', code: 'REF',address:'Endereço', 
+                                      cep: '25000-000', city: 'Recife', area: 1000,
+                                      description: 'Alguma coisa')
+                                    
+      second_warehouse = Warehouse.new(name: 'Recife', code: 'REE',address:'Avenida', 
+                                        cep: '35000-000', city: 'Recife', area: 1500,
+                                        description: 'Outra coisa')
+
+      # Act
+      result = second_warehouse.valid?
+      # Assert
+      expect(result).to eq false
+
+    end
+
+    it 'verdadeiro quando formato do CEP correto' do
+      # Arrange
+      warehouse = Warehouse.create(name: 'Rio', code: 'RIO', address:'Av. Atlantica, 1200',
+                                cep: '00000-000', city: 'Rio', area: 40000,
+                                description: 'Galpão do aeroporto do Rio')
+
+      # Act
+      result = warehouse.valid?
+
+      # Act
+      expect(result).to eq true
+    end
+
+  end
 end
